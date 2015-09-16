4117fe47f3150c99a9a7472c9788859775f22494
Nick Bucheleres <nick.bucheleres@gmail.com>
2015-09-16 10:54:39 -0700

    photo fades on scroll

--- client/home.js
+++ client/home.js
@@ -3,3 +3,32 @@
 Router.route('homeTemplate', {
   path: '/'
 })
+
+Template.homeTemplate.events({
+  'click #header': function(event, template){
+    event.preventDefault()
+
+    const scrollTop = $('body').scrollTop()
+
+    console.log('scrollTop', scrollTop)
+
+  }
+})
+
+Template.homeTemplate.onCreated(function(){
+  console.log('on created')
+
+  $(document).on('scroll', function(event){
+
+    const scrollTop = $('body').scrollTop()
+
+    console.log('on scroll', scrollTop)
+
+    if(scrollTop > 300){
+      $('#header').fadeOut()
+    } else if(scrollTop < 300) {
+      // $('#header').fadeIn()
+    }
+  })
+
+})
