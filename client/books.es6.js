e6ee7b7f8f50332e6c9a30bbc7103d5c7bd53873
Nick Bucheleres <nick.bucheleres@gmail.com>
2015-10-14 17:48:50 -0700

    compressed images

--- client/books.es6.js
+++ client/books.es6.js
@@ -10,26 +10,3 @@ Template.booksTemplate.helpers({
     return true
   }
 })
-
-// SeoCollection.update(
-//     {
-//         route_name: 'books'
-//     },
-//     {
-//         $set: {
-//             route_name: 'books',
-//             title: 'Bill Diffenderffer--Books',
-//             meta: {
-//                 'description': 'Bill Diffenderffer is a science fiction, sci-fi, quantum
-//                 physics, spirituality author'
-//             },
-//             og: {
-//                 'title': 'Books - Bill Diffenderffer',
-//                 'image': 'http://manuel-schoebel.com/images/authors/manuel-schoebel.jpg'
-//             }
-//         }
-//     },
-//     {
-//         upsert: true
-//     }
-// );
